353f7979d4212ce94fb8fc7022b13913ae3da5d6
chesroncjr <chesron.coutinho@student.rocva.nl>
2021-04-14 14:09:43 +0200

    php 4 froms 1 en 2

---
+++ 4-Functions/taak01/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+function schrijfkleur($kleur0){
+    echo "<p style='color:$kleur0'>dit is de kleur $kleur0</p>";
+}
+schrijfkleur("red")  ;
+function schrijfkleur1($kleur1){
+    echo "<p style='color:$kleur1'>dit is de kleur $kleur1</p>";
+}
+schrijfkleur1("yellow")  ;
+
+function schrijfkleur2($kleur2){
+    echo "<p style='color:$kleur2'>dit is de kleur $kleur2</p>";
+}
+schrijfkleur2("blue")  ;
+function schrijfkleur3($kleur3){
+    echo "<p style='color:$kleur3'>dit is de kleur $kleur3</p>";
+}
+schrijfkleur2("orange")  ;
+?>
+
+
+
+</body>
+</html> 
---
+++ 4-Functions/taak02/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+
+function drieParameters ($een, $twee, $drie)
+{}
+  return $een + $twee;
+
+echo drieParameters (1, 2, 3) + drieParameters (4, 5, 3)
+
+?>
+</body>
+</html> 
